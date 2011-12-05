4d4aaaa324c10005a6f78032c87c568e6e3d13d1
Łukasz Dziedziul <luk@lukasz.companyc.clovin.com.pl>
2011-12-05 22:56:02 +0100

    add scaffolding author controller

---
+++ grails-app/controllers/org/jteam/AuthorController.groovy
@@ -0,0 +1,6 @@
+package org.jteam
+
+class AuthorController {
+
+   def scaffold = true
+}
---
+++ test/unit/org/jteam/AuthorControllerTests.groovy
@@ -0,0 +1,17 @@
+package org.jteam
+
+import grails.test.ControllerUnitTestCase
+
+class AuthorControllerTests extends ControllerUnitTestCase {
+   protected void setUp() {
+      super.setUp()
+   }
+
+   protected void tearDown() {
+      super.tearDown()
+   }
+
+   void testSomething() {
+
+   }
+}
