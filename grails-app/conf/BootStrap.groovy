459eedb1f1c99f6e100761a6c51da56d8a440bca
Łukasz Dziedziul <luk@lukasz.companyc.clovin.com.pl>
2011-12-06 13:19:00 +0100

    added bootstrap domain objects creation

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,13 @@
+import org.jteam.Author
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+   def init = { servletContext ->
+      new Author(firstname: "Adam", lastname: "Mickiewicz", age: 123, nip: 1231432).save()
+      new Author(firstname: "Henryk", lastname: "Sienkiewicz", age: 123, nip: 1234312).save()
+      new Author(firstname: "Julian", lastname: "Słowacki", age: 123, nip: 132121).save()
+      new Author(firstname: "Sławomir", lastname: "Słowik", age: 123, nip: 122312).save()
+   }
+   def destroy = {
+   }
 }
